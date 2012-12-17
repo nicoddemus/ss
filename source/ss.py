cdbb6def97ff8c3270724950b0a33038b14eff81
Bruno Oliveira <nicoddemus@gmail.com>
2012-12-17 04:19:28 -0200

    moved hash function to its own module.

--- source/ss.py
+++ source/ss.py
@@ -1,35 +1,7 @@
 import xmlrpclib
-import struct, os, pprint, difflib
-
-def HashFile(name): 
-    longlongformat = 'q'  # long long 
-    bytesize = struct.calcsize(longlongformat) 
-        
-    f = open(name, "rb") 
-        
-    filesize = os.path.getsize(name) 
-    hash = filesize 
-        
-    if filesize < 65536 * 2: 
-        return "SizeError" 
-     
-    for x in range(65536/bytesize): 
-        buffer = f.read(bytesize) 
-        (l_value,)= struct.unpack(longlongformat, buffer)  
-        hash += l_value 
-        hash = hash & 0xFFFFFFFFFFFFFFFF #to remain as 64bit number  
-             
-
-    f.seek(max(0,filesize-65536),0) 
-    for x in range(65536/bytesize): 
-        buffer = f.read(bytesize) 
-        (l_value,)= struct.unpack(longlongformat, buffer)  
-        hash += l_value 
-        hash = hash & 0xFFFFFFFFFFFFFFFF 
-     
-    f.close() 
-    returnedhash =  "%016x" % hash 
-    return returnedhash, filesize 
+import difflib
+import os
+from calculate_hash import CalculateHashForFile
 
 
 uri = 'http://api.opensubtitles.org/xml-rpc'
@@ -41,7 +13,8 @@ token = login_info['token']
 try:
     filename = r'M:\Incoming\The.Big.Bang.Theory.S06E11.HDTV.x264-LOL.mp4'
     
-    moviehash, moviebytesize = HashFile(filename)
+    moviehash = CalculateHashForFile(filename)
+    moviebytesize = os.path.getsize(filename)
     search_input = dict(
         moviehash=moviehash,
         moviebytesize=moviebytesize,
@@ -54,7 +27,6 @@ try:
         print '-' * 80
         keys = ['SeriesSeason', 'MovieName', 'LanguageName', 'SubFileName', 'SubDownloadLink']
         for key in keys:
-            
             print key + ':', search_result[key]
         possibilities.append(search_result['SubFileName'])
             
