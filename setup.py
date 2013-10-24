bbdc674d80058886d306719e57710a37ddaa8de0
Bruno Oliveira <nicoddemus@gmail.com>
2013-10-23 22:25:41 -0200

    formatting setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 description = "Command line script that automatically searches for video subtitles using OpenSubtitles.org APIs."
 long_description = ''
 
 setup(
-    name = "ss",
-    version = "1.2.0",
-    packages = [],
-    scripts = ['ss.py'],
+    name="ss",
+    version="1.2.0",
+    packages=[],
+    scripts=['ss.py'],
     py_modules=['calculate_hash'],
     install_requires=[x.strip() for x in file('requirements.txt')],
-    entry_points = {'console_scripts' : ['ss = ss:Main']},
-    
+    entry_points={'console_scripts': ['ss = ss:Main']},
+
     # metadata for upload to PyPI
-    author = "nicoddemus@gmail.com",
-    author_email = "nicoddemus@gmail.com",
-    description = description,
-    long_description = long_description,
-    license = "GPL",
-    keywords = "subtitles script",
-    url = "http://nicoddemus.github.io/ss/",
+    author="nicoddemus@gmail.com",
+    author_email="nicoddemus@gmail.com",
+    description=description,
+    long_description=long_description,
+    license="GPL",
+    keywords="subtitles script",
+    url="http://nicoddemus.github.io/ss/",
 )
